75c024bbc4a87c1a466ac7396dbe8a1ec890c0c2
Gusky <vasile-adrian.turcanu@student.tuiasi.ro>
2022-08-16 00:23:33 +0300

    updated Answers element visuals

--- src/GamePage/Answers.js
+++ src/GamePage/Answers.js
@@ -4,13 +4,13 @@ const Answers = (props) => {
 
     const answers = props.chatAnswers.map((answer, index) => {
         const percentage = Math.round(answer[1]*100/pointsSum);
-        const correctAnswerStyle={background: 'linear-gradient(to right, #79797b ' + percentage +'%, #3a3a3c 0%'};
+        const correctAnswerStyle={background: 'linear-gradient(to right, #9147ff ' + percentage +'%, #3a3a3c 0%'};
         const wrongAnswerStyle={textAlign: 'center', fontWeight: '100'};
 
         if (playerAnswers.includes(answer[0]))
             return <div style={correctAnswerStyle} key={index}>{answer[0]} <span>{percentage}% ({answer[1]})</span></div>
         else
-            return <div style={wrongAnswerStyle} key={index}>{index}</div>
+            return <div style={wrongAnswerStyle} key={index}>{index+1}</div>
     })
 
     return (<div id='answersContainer'>
