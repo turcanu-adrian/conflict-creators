b7bd655bbeb56bac36730856d58573cac2843710
Gusky <vasile-adrian.turcanu@student.tuiasi.ro>
2022-08-16 23:33:20 +0300

    fixed Player Answer Phase game logic

--- src/GameModes/Phases/PlayerAnswerPhase.js
+++ src/GameModes/Phases/PlayerAnswerPhase.js
@@ -3,6 +3,7 @@ import { Timer } from "../../GamePage/Timer.js";
 import {Answers} from "../../GamePage/Answers.js";
 import {PlayerProfile} from "../../GamePage/PlayerProfile";
 import {PlayerStats} from "../../GamePage/PlayerStats";
+import { useState } from "react";
 
 function playerAnswerPhaseFunction(tags, message, gameVars, addAnswer){
     const validMessage=message.startsWith('!answer') && tags['display-name'] === gameVars['chatPlayer']
@@ -11,21 +12,27 @@ function playerAnswerPhaseFunction(tags, message, gameVars, addAnswer){
 }
 
 const PlayerAnswerPhase = (props) => {
+    const [lastAnswer, setLastAnswer]=useState(undefined);
     let revealedAnswers = 0;
     props.gameVars['chatAnswers'].forEach(answer => {
         if (props.gameVars['playerAnswers'].includes(answer[0]))
             revealedAnswers++;
     }); 
 
-    if (revealedAnswers === props.gameVars['chatAnswers'].length)
-        setTimeout(timerFinish, 2000);
+    if (revealedAnswers === props.gameVars['chatAnswers'].length){
+            props.gameVars['chatAnswers'].forEach(answer => {
+                if (props.gameVars['playerAnswers'].includes(answer[0]))
+                    props.gameVars['playerStats'][props.gameVars['currentPlayer']]['roundPoints']+=answer[1];
+            }); 
+            props.updatePhase('roundEnd');
+}
+    else if (lastAnswer !== props.gameVars['playerAnswers'][props.gameVars['playerAnswers'].length-1])
+        setLastAnswer(props.gameVars['playerAnswers'][props.gameVars['playerAnswers'].length-1])
 
     function timerFinish(){
-        props.gameVars['chatAnswers'].forEach(answer => {
-            if (props.gameVars['playerAnswers'].includes(answer[0]))
-                props.gameVars['playerStats'][props.gameVars['currentPlayer']]['roundPoints']+=answer[1];
-        }); 
-        props.updatePhase('roundEnd');
+        if (props.gameVars['playerAnswers'][props.gameVars['playerAnswers'].length-1] === lastAnswer)
+            props.addAnswer(lastAnswer+'fail', props.gameVars['currentPlayer']);
+        setLastAnswer(props.gameVars['playerAnswers'][props.gameVars['playerAnswers'].length-1])
     }
 
     const chattingImg = new Image().src=process.env.REACT_APP_CHATTING_EMOTE;
@@ -34,7 +41,7 @@ const PlayerAnswerPhase = (props) => {
     return (<>
         <PlayerProfile chatPlayer={props.gameVars['chatPlayer']}/>
         <PlayerStats playerStats={props.gameVars['playerStats']}/>
-        <Timer timerFinish={timerFinish} timerStart={99}/>
+        <Timer key={lastAnswer} timerFinish={timerFinish} timerStart={20}/>
         <div className="centeredText">{props.gameVars['currentQuestion']}<br/>Chat player must answer with !answer (Example: !answer forsen)</div>
         <StreamerInput gameVars={props.gameVars} addAnswer={props.addAnswer}/>
         <Answers gameVars={props.gameVars}/>
