9ae1b5faabb2443c9bb19cb26e0803ee870a8a1a
Gusky <vasile-adrian.turcanu@student.tuiasi.ro>
2022-08-18 01:47:30 +0300

    added site logo

--- src/GamePage/MainMenu.js
+++ src/GamePage/MainMenu.js
@@ -1,7 +1,9 @@
 const MainMenu = (props) => {
-    
+    const siteLogo = new Image().src = process.env.REACT_APP_SITE_LOGO;
+
     return (<>
         <div className='centeredText'>
+            <img src={siteLogo} width='70%' alt='siteLogo'/>
             <div>Choose gamemode</div>
             <button onClick={() => props.changeState('soloGame')}>Parasocial Confrontation</button>
         </div>
--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -4,11 +4,15 @@ const LoginPage = () =>
 {
     const accessToken = new URLSearchParams(window.location.hash.slice(1)).get('access_token'); //EXTRACT ACCESS TOKEN FROM LINK
     const twitchLogo = new Image().src=process.env.REACT_APP_TWITCH_LOGO;
+    const siteLogo = new Image().src=process.env.REACT_APP_SITE_LOGO;
 
     if (accessToken) //IF LINK CONTAINS ACCESS TOKEN
         getProfileData(accessToken);
 
-    return <a href={process.env.REACT_APP_AUTH_URL}><div id='loginButton'><img alt='twitchLogo' style={{width: 'auto', maxHeight: '4vw', float: 'left'}} src={twitchLogo}/>Login with Twitch</div></a>;
+    return (<>
+        <div className="centeredText"><img src={siteLogo} width='70%' alt='siteLogo'/></div>
+        <a href={process.env.REACT_APP_AUTH_URL}><div id='loginButton'><img alt='twitchLogo' style={{width: 'auto', maxHeight: '4vw', float: 'left'}} src={twitchLogo}/>Login with Twitch</div></a>
+    </>)
 }
 
 
